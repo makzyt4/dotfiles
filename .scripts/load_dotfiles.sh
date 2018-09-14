a1f1d3db225d45385d631d1529f718a22a15c01d
Maksymilian Zytkiewicz <maksymilian.zytkiewicz@gmail.com>
2018-09-14 20:52:42 +0200

    First commit

---
+++ .scripts/load_dotfiles.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cp -r ~/dotfiles/.config ~/
+cp -r ~/dotfiles/.mpdscribble ~/
+cp -r ~/dotfiles/.ncmpcpp ~/
+cp -r ~/dotfiles/.scripts ~/
+cp -r ~/dotfiles/.urxvt ~/
+cp -r ~/dotfiles/.wallpapers ~/
+cp ~/dotfiles/.fehbg ~/
+cp ~/dotfiles/.vimrc ~/
+cp ~/dotfiles/.xinitrc ~/
+cp ~/dotfiles/.Xresources ~/
+cp ~/dotfiles/.zshrc ~/
---
+++ .scripts/prepare_dotfiles.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+mkdir ~/dotfiles
+mkdir -p ~/dotfiles/.config
+cp -r ~/.config/htop ~/dotfiles/.config
+cp -r ~/.config/i3 ~/dotfiles/.config
+cp -r ~/.config/mpd ~/dotfiles/.config
+cp -r ~/.config/neofetch ~/dotfiles/.config
+cp -r ~/.config/polybar ~/dotfiles/.config
+cp -r ~/.config/ranger ~/dotfiles/.config
+cp -r ~/.config/rofi ~/dotfiles/.config
+cp -r ~/.mpdscribble/ ~/dotfiles
+cp -r ~/.ncmpcpp ~/dotfiles
+cp -r ~/.scripts ~/dotfiles
+cp -r ~/.urxvt ~/dotfiles
+cp -r ~/.wallpapers ~/dotfiles
+cp ~/.fehbg ~/dotfiles
+cp ~/.vimrc ~/dotfiles
+cp ~/.xinitrc ~/dotfiles
+cp ~/.Xresources ~/dotfiles
+cp ~/.zshrc ~/dotfiles
